575f0856d43ebb64b49007570ab1a396cc37f7cd
nauua <nasir.auua@aurea.com>
2018-12-01 08:37:38 +0500

    add more cases

--- src/main/java/com/aurea/nauua/paramcheck/ExampleClass.java
+++ src/main/java/com/aurea/nauua/paramcheck/ExampleClass.java
@@ -5,29 +5,44 @@ import java.util.Objects;
 public class ExampleClass {
 
 	public String name;
-	
-	public ExampleClass() {}
-	
+
+	public ExampleClass() {
+	}
+
 	public ExampleClass(String stringVal) {
 		this.name = Objects.requireNonNull(stringVal);
 	}
-	
-	public void assignNewValue(String input) {
+
+	// single param instance method
+	public void method1(String input) {
 		Objects.requireNonNull(input);
 		this.name = "Nasir";
 	}
-	
-	public void assignNewValue2(String input) {
+
+	// single param static method
+	public static void method2(String input) {
 		Objects.requireNonNull(input);
 	}
 	
-	
-	public void requireNotNullOnSecondLine(String input) {
-		this.name = "Nasir2";
+	// multiple param instance method
+	public void method1(String input, String input2) {
 		Objects.requireNonNull(input);
+		this.name = "Nasir";
 	}
 
-	public void requireNotNullAbsent(String input) {
+	// multiple param static method
+	public static void method2(String input, String input2) {
+		Objects.requireNonNull(input);
+	}
+	
+	// discard this method
+	public void method0() {
 		this.name = "Nasir2";
 	}
+
+	// discard this method
+	public void method01() {
+		this.name = "Nasir2";
+	}
+
 }
